0a3f6af64f0a6b2c6aa939c7e1e353499264db50
marc7hedev <driberstagg@gmail.com>
2023-12-28 13:41:00 -0600

    Tabla de proyectos implementada y ajustada a la interfaz

--- portafolio.php
+++ portafolio.php
@@ -1,7 +1,55 @@
 <?php include "cabecera.php"; ?>
 
+<!-- Contenedor del formulario y tabla -->
+<div class="container">
+    <div class="row">
+        
+        <!-- Columna de Formulario -->
+        <div class="col-md-5">
+            <div class="card">
+                <div class="card-header">Datos del proyecto</div>
+                <div class="card-body">
+                    <form action="portafolio.php" method="post">
+                        Nombre del proyecto: <input class="form-control" type="text" name="nombre" id="">
+                        <br>
+                        Imagen del proyecto: <input class="form-control" type="file" name="archivo" id="">
+                        <br>
+                        <input class="btn btn-success" type="submit" value="Enviar">
+                    </form>
+                </div>
+            </div>
+            
+        </div>
+        
+        <!-- Columna de Tabla -->
+        <div class="col-md-7">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Imagen</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <tr>
+                        <td>1</td>
+                        <td>Aplicación web</td>
+                        <td>imagen.jpg</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+            
+        </div>
+        
+    </div>
+</div>
 
-    <p>Estás en el portafolio</p>
+
+
+
+
 
 
 
