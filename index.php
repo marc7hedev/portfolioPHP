82f644ae3cee2aee2f73f68892b90571831a5353
marc7hedev <driberstagg@gmail.com>
2023-12-27 22:47:21 -0600

    Implementando Bootstrap 5

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include "cabecera.php"; ?>
+
+
+    <div class="p-5 bg-light">
+        <div class="container">
+            <h1 class="display-3">Bienvenidos</h1>
+            <p class="lead">a mi portafolio hecho con PHP y BS5!</p>
+            <hr class="my-2">
+            <p>Más información</p>
+        </div>
+    </div>
+    
+
+
+
+<?php include "pie.php"; ?>
---
+++ portafolio.php
@@ -0,0 +1,8 @@
+<?php include "cabecera.php"; ?>
+
+
+    <p>Estás en el portafolio</p>
+
+
+
+<?php include "pie.php"; ?>
